fe0034e58e7c9fea89d3d197012647efb0f0e843
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-05-22 22:14:44 -0400

    Done.

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -15,4 +15,26 @@
  
  */
 
+-(NSArray *)arrayWithFibonacciSequenceToIndex:(NSUInteger)index {
+    
+    NSMutableArray *sequence = [[NSMutableArray alloc]init];
+    for (NSUInteger i = 0; i < index + 1; i++) {
+        if (i == 0) {
+            [sequence addObject: @0];
+        }
+        else if (i == 1) {
+            [sequence addObject:@1];
+        }
+        else {
+            
+            NSInteger first = [[sequence lastObject]integerValue];
+            NSInteger second = [[sequence objectAtIndex:[sequence count]-2]integerValue];
+            NSNumber *subtotal = @(first + second);
+            [sequence addObject:subtotal];
+        }
+    }
+    NSLog (@"%@",sequence);
+    return sequence;
+}
+
 @end
